1988eb46935e5194e96b307fcdb364024f850d55
Kyle Aoki <vk1997489@gmail.com>
2020-11-11 09:24:47 -0800

    adds commented-out axios-config

--- src/util/axiosConfig.ts
+++ src/util/axiosConfig.ts
@@ -2,5 +2,8 @@ import Axios from "axios";
 
 export const axiosInstance = Axios.create({
   baseURL: "http://localhost:8080/SDNetwork",
-  
 });
+
+// export const axiosInstance = Axios.create({
+//   baseURL: "", // we will put the EC2 URL in here before deployment
+// });
