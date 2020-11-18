aba543ac423fdca6fa8c6a900721278528b6bfa1
Kyle Aoki <69960581+kyle-aoki@users.noreply.github.com>
2020-11-18 14:22:06 -0800

    Update axiosConfig.ts

--- src/util/axiosConfig.ts
+++ src/util/axiosConfig.ts
@@ -2,6 +2,6 @@ import Axios from "axios";
 
 export const axiosInstance = Axios.create({
 
-  baseURL: "http://3.138.193.161:8080/SDNetwork-0.0.1-SNAPSHOT",
+  baseURL: "http:///SDNetwork-0.0.1-SNAPSHOT",
 
 });
